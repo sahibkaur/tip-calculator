f427fe890fa2ec33aa373b203d8b81ed7bce8238
sahibkaur <sahibkaur81@gmail.com>
2023-06-19 01:07:00 +0530

    change the reset button to active state when value in input is ore than 0

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const reset_button = document.getElementById('reset');
 console.log("Hi");
 console.log(reset_button);
+const input_val = document.getElementsByClassName('inputs');
+console.log(input_val);
+function toActive() {
+    if(input_val[0].value>0 && input_val[1].value>0) {
+        reset_button.classList.add("active");
+    }
+}
+
