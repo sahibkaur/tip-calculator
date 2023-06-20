b9693f9024e97690d7b7ca3d9b1ee5b97dd22c2f
sahibkaur <sahibkaur81@gmail.com>
2023-06-20 16:51:15 +0530

    update reset button functionality

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const reset_button = document.getElementById('reset');
-console.log("Hi");
-console.log(reset_button);
 const input_val = document.getElementsByClassName('inputs');
 let tipped = document.getElementById('tip-amount');
 let totalper = document.getElementById('total-amount');
@@ -26,6 +24,7 @@ function toActive() {
 
     if(input_val[0].valueAsNumber>0 && input_val[1].valueAsNumber>0 && flag == 1 ) {
         reset_button.classList.add("active");
+        reset_button.classList.remove("inactive");
         console.log(per, input_val[0].valueAsNumber);
         tip_amount = (input_val[0].valueAsNumber)*per/100;
         console.log(tip_amount)
@@ -40,17 +39,24 @@ function toActive() {
 
     }
 }
+function inactiveButtons() {
+    tip_pointer = tip_buttons[0];
+    let i=0;
+    while(tip_pointer!=null) {
+        tip_pointer.classList.remove('active');
+        i++;
+        tip_pointer = tip_buttons[i];
+    }
+}
 
 function changeState(selectedTip) {
     console.log(selectedTip);
     tip_pointer = tip_buttons[0];
     let i=0;
     let custom_tip;
-    while(tip_pointer!=null) {
-        tip_pointer.classList.remove('active');
-        i++;
-        tip_pointer = tip_buttons[i];
-    }
+
+    inactiveButtons();
+
     selectedTip.classList.add('active');
     per = selectedTip.value;
     if(per=="custom") {
@@ -60,3 +66,17 @@ function changeState(selectedTip) {
     }     
     toActive();
 }
+
+function reset(btn) {
+    if(btn.classList.contains('inactive')) {
+        return;
+    }
+    input_val[0].value="";
+    input_val[1].value="";
+    inactiveButtons();
+    tipped.innerHTML = "$0.00";
+    totalper.innerHTML = "$0.00";
+    btn.classList.remove("active");
+    btn.classList.add("inactive");
+}
+
